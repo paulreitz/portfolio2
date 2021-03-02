de5c3512364f2c44e6983c0591205ac22e0e5687
paulreitz <avenose@gmail.com>
2021-03-02 08:52:13 -0800

    Added a description to the projects page. Removed the default readme text.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/styles';
 
@@ -18,6 +19,9 @@ const useStyles = makeStyles(theme => ({
     title: {
         textAlign: 'center',
         marginBottom: 15
+    },
+    description: {
+        padding: 15
     }
 }))
 
@@ -28,6 +32,14 @@ export default function Projects(props) {
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <Typography variant="h4" className={classes.title}>Projects</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Box className={classes.description}>
+                        <Typography variant="body2">
+                            These are projects I created either to fill a need I had, or to explore some concept in a simple context. 
+                            Due to the costs involved, if the project requires a database then there is no live example of the project.
+                        </Typography>
+                    </Box>
                 </Grid>
                 {
                     projectData.map(item => (
